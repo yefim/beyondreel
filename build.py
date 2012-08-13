64a7f0a80910f5e2e55ca8e6970f5e9ed1ad6a26
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-08-13 15:33:46 -0400

    added more templates

--- build.py
+++ build.py
@@ -11,9 +11,29 @@ def build_template(env, template_name, **kwargs):
 def build_index(env):
     build_template(env, 'index.html')
 
+def build_story(env):
+    build_template(env, 'story.html')
+
+def build_crew(env):
+    build_template(env, 'crew.html')
+
+def build_films(env):
+    build_template(env, 'films.html')
+
+def build_gallery(env):
+    build_template(env, 'gallery.html')
+
+def build_contact(env):
+    build_template(env, 'contact.html')
+
 def main():
     env = Environment(loader=FileSystemLoader(searchpath="./templates"))
     build_index(env)
+    build_story(env)
+    build_crew(env)
+    build_films(env)
+    build_gallery(env)
+    build_contact(env)
 
 if __name__ == '__main__':
     main()
